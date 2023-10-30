60057963d04e2b3e0d0145a60aef28975183d5ea
Tomás Macri <tomas.macri@vasscompany.com>
2023-10-30 09:34:34 +0100

    Integrate the RemoteDataSource with QuotesRepository

--- poc-kmm tomas/shared/src/commonMain/kotlin/com/vass/example/kmmapp/data/repository/QuotesRepositoryImpl.kt
+++ poc-kmm tomas/shared/src/commonMain/kotlin/com/vass/example/kmmapp/data/repository/QuotesRepositoryImpl.kt
@@ -2,16 +2,21 @@ package com.vass.example.kmmapp.data.repository
 
 import com.vass.example.kmmapp.data.local.datastore.LocalDataSource
 import com.vass.example.kmmapp.data.model.Quote
+import com.vass.example.kmmapp.data.remote.datastore.RemoteDataSource
 
 internal class QuotesRepositoryImpl(
+    private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource
 ): QuotesRepository {
     override suspend fun getQuotes(): List<Quote> {
         return localDataSource.getQuotes()
     }
 
-    override suspend fun insert(newQuote: Quote) {
-        return localDataSource.insert(newQuote)
+    override suspend fun insert() {
+        val newQuote = remoteDataSource.getQuote()
+        newQuote?.let {
+            localDataSource.insert(it)
+        }
     }
 
     override suspend fun resetQuotes() {
