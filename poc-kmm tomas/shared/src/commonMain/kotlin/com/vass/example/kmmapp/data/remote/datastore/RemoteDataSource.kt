140b49c11429b341fdcbad60bf51ac87f5f78ae3
Tomás Macri <tomas.macri@vasscompany.com>
2023-10-30 17:12:07 +0100

    Apply KMMLogger to data classes

--- poc-kmm tomas/shared/src/commonMain/kotlin/com/vass/example/kmmapp/data/remote/datastore/RemoteDataSource.kt
+++ poc-kmm tomas/shared/src/commonMain/kotlin/com/vass/example/kmmapp/data/remote/datastore/RemoteDataSource.kt
@@ -3,6 +3,7 @@ package com.vass.example.kmmapp.data.remote.datastore
 import com.vass.example.kmmapp.data.model.Quote
 import com.vass.example.kmmapp.data.remote.api.model.QuoteDTO
 import com.vass.example.kmmapp.data.remote.api.model.toQuote
+import com.vass.example.kmmapp.utils.logger.KMMLogger
 import com.vass.example.kmmappp.BuildKonfig
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -18,19 +19,36 @@ internal interface RemoteDataSource {
 
 internal class QuotesApiRemoteDataSource(
     private val ktorClient: HttpClient,
-    private val dispatcher: CoroutineDispatcher
+    private val dispatcher: CoroutineDispatcher,
+    private val logger: KMMLogger
 ) : RemoteDataSource {
+    companion object {
+        private const val TAG = "RemoteDataSource"
+    }
+
     override suspend fun getQuote(): Quote? {
         return withContext(dispatcher) {
-            val response: List<QuoteDTO> = ktorClient.get("${BuildKonfig.apiUrl}quotes").body()
-            response.firstOrNull()?.toQuote()
+            val apiCallUrl = "${BuildKonfig.apiUrl}quotes"
+            logger.debug(TAG, "[getQuote] --> $apiCallUrl")
+            val response = ktorClient.get(apiCallUrl)
+            with(response) {
+                val newQuoteAsList: List<QuoteDTO> = body()
+                logger.debug(TAG, "[getQuote] <-- $status")
+                logger.debug(TAG, "[getQuote] content <-- $newQuoteAsList ")
+                newQuoteAsList.firstOrNull()?.toQuote()
+            }
         }
     }
 
     override suspend fun getQuotes(amount: Int): List<Quote> {
         return withContext(dispatcher) {
-            val response: List<QuoteDTO> = ktorClient.get("${BuildKonfig.apiUrl}quotes/$amount").body()
-            response.map { it.toQuote() }
+            val response = ktorClient.get("${BuildKonfig.apiUrl}quotes/$amount")
+            with(response) {
+                val newQuotes: List<QuoteDTO> = body()
+                logger.debug(TAG, "[getQuotes] <-- $status")
+                logger.debug(TAG, "[getQuotes] content <-- $newQuotes")
+                newQuotes.map { it.toQuote() }
+            }
         }
     }
 
